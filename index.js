64dc98f3e41efb1b068b7584b2f74926deb569ee
Laura <lauragutierrezgallud@gmail.com>
2023-11-07 20:53:23 +0100

    clase js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Buscar y guardar en una variable todos los elementos img de mi web. De esta forma, busca todos los elementos de la web que tengan la caracteristica entre '', en este caso img
+
+cont myImgs = document.querySelectorAll('img')
+
+console.log(myImgs)
+
+// Recorrer todos los elements img encontrados y cambiarles el src
+
+myImgs.array.forEach(imgNode => {
+    
+});
